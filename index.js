020b32996b6967a2b5bd96661603f9f4f302aad1
mdre797 <markdrecoll@gmail.com>
2021-04-20 20:42:14 -0500

    Got file creation working. Got user inputs working.

--- index.js
+++ index.js
@@ -3,71 +3,77 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'Title of repository: ',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Description for the repository: ',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'Table of contents: ',
+        name: 'tableOfContents'
+    },
+    {
+        type: 'input',
+        message: 'Installation: ',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'Usage: ',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'License: ',
+        name: 'license'
+    },
+    {
+        type: 'input',
+        message: 'Contributing: ',
+        name: 'contributing'
+    },
+    {
+        type: 'input',
+        message: 'Tests: ',
+        name: 'tests'
+    },
+    {
+        type: 'input',
+        message: 'Questions: ',
+        name: 'questions'
+    },
+    {
+        type: 'list',
+        message: 'What is your license?',
+        name: 'license',
+        choices: ['Email', 'Text', 'Phone Call']
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-
-    inquirer
-        .prompt([
-            {
-                type: 'input',
-                message: 'Title of repository: ',
-                name: 'title'
-            },
-            {
-                type: 'input',
-                message: 'Description for the repository: ',
-                name: 'description'
-            },
-            {
-                type: 'input',
-                message: 'Table of contents: ',
-                name: 'tableOfContents'
-            },
-            {
-                type: 'input',
-                message: 'Installation: ',
-                name: 'installation'
-            },
-            {
-                type: 'input',
-                message: 'Usage: ',
-                name: 'usage'
-            },
-            {
-                type: 'input',
-                message: 'License: ',
-                name: 'license'
-            },
-            {
-                type: 'input',
-                message: 'Contributing: ',
-                name: 'contributing'
-            },
-            {
-                type: 'input',
-                message: 'Tests: ',
-                name: 'tests'
-            },
-            {
-                type: 'input',
-                message: 'Questions: ',
-                name: 'questions'
-            }
-        ])
-        .then((response) => {
-            const filename = `${response.title.toLowerCase().split('').join('')}.md`;
-
-            fs.writeFile(filename, JSON.stringify(response, null, '\t'), (err) =>
-            err ? console.error(err) : console.log('Success!')
-            );
-        }
+function writeToFile(theFileName, data) {
+    fs.writeFile(theFileName, JSON.stringify(data, null, '\t'), (err) =>
+        err ? console.error(err) : console.log('Success!')
     );
 }
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((response) => {
+            const filename = `${response.title.toLowerCase().split('').join('')}.md`;
+            writeToFile(filename, response);
+        }
+    );
+}
 
 // Function call to initialize app
 init();
